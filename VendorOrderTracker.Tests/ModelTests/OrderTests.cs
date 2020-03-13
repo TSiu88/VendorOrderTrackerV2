3e3fc06dd68059fa2e538ac3deeaabb96fd85a56
Tiffany Siu <tsiu88@gmail.com>
2020-03-13 10:10:47 -0700

    Add all tests, ensure all tests pass

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrders.Models;
 using System;
+using System.Collections.Generic;
 
 namespace VendorOrders.Tests
 {
@@ -17,14 +18,40 @@ namespace VendorOrders.Tests
     {
       Order newOrder = new Order("title", "description", "date", 5);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+      Assert.AreEqual("title", newOrder.Title);
+      Assert.AreEqual("description", newOrder.Description);
+      Assert.AreEqual("date", newOrder.Date);
+      Assert.AreEqual(5, newOrder.Price);
     }
 
-    // [TestMethod]
-    // public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
-    // {
-    //   Order newOrder = new Order("property");
-    //   string result = newOrder.Property;
-    //   Assert.AreEqual("Expected", result);
-    // }
+    [TestMethod]
+    public void GetAll_ReturnAllOrdersInList_ListOfOrders()
+    {
+      Order newOrder1 = new Order("title", "description", "date", 5);
+      Order newOrder2 = new Order("title", "description", "date", 5);
+      Order newOrder3 = new Order("title", "description", "date", 5);
+      List<Order> expectedOrders = new List<Order> {newOrder1, newOrder2, newOrder3};
+      CollectionAssert.AreEquivalent(expectedOrders, Order.GetAll());
+    }
+
+    [TestMethod]
+    public void ClearAll_ClearAllOrdersInList_EmptyListOfOrders()
+    {
+      Order newOrder1 = new Order("title", "description", "date", 5);
+      Order newOrder2 = new Order("title", "description", "date", 5);
+      Order newOrder3 = new Order("title", "description", "date", 5);
+      List<Order> expectedOrders = new List<Order> {};
+      Order.ClearAll();
+      CollectionAssert.AreEquivalent(expectedOrders, Order.GetAll());
+    }
+
+    [TestMethod]
+    public void Find_FindOrderInListById_Order2()
+    {
+      Order newOrder1 = new Order("title", "description", "date", 5);
+      Order newOrder2 = new Order("title", "description", "date", 5);
+      Order newOrder3 = new Order("title", "description", "date", 5);
+      Assert.AreSame(newOrder2, Order.Find(2));
+    }
   }
 }
