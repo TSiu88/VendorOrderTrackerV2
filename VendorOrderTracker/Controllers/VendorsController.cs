a531920e912f2eb8ecea88681a231ee7bc45542c
Tiffany Siu <tsiu88@gmail.com>
2022-12-30 22:09:14 -0800

    Add starting seed data, add more fields for vendors, sort orders by date

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using VendorOrders.Models;
 using System.Collections.Generic;
+using System;
 
 namespace VendorOrders.Controllers
 {
@@ -9,6 +10,10 @@ namespace VendorOrders.Controllers
     [HttpGet("/vendors")]
     public ActionResult Index() 
     { 
+      if (Vendor.VendorCount() == 0)
+      {
+        Vendor.LoadInitialData();
+      }
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
@@ -17,9 +22,9 @@ namespace VendorOrders.Controllers
     public ActionResult New() { return View(); }
 
     [HttpPost("/vendors")]
-    public ActionResult Create(string name, string description) 
+    public ActionResult Create(string name, string description, string address, string phone, string email) 
     { 
-      Vendor myVendor = new Vendor(name, description);
+      Vendor myVendor = new Vendor(name, description, address, phone, email);
       return RedirectToAction("Index"); 
     }
 
@@ -29,8 +34,10 @@ namespace VendorOrders.Controllers
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(id);
       List<Order> vendorOrders = foundVendor.Orders;
+      Dictionary<int, DateTime> orderDates = foundVendor.SortedDates;
       model.Add("orders", vendorOrders);
       model.Add("vendor", foundVendor);
+      model.Add("dates", orderDates);
       return View(model); 
     }
 
@@ -42,8 +49,10 @@ namespace VendorOrders.Controllers
       Order newOrder = new Order(title, description, date, price);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
+      Dictionary<int, DateTime> orderDates = foundVendor.SortedDates;
       model.Add("orders", vendorOrders);
       model.Add("vendor", foundVendor);
+      model.Add("dates", orderDates);
       return View("Show", model);
     }
   }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace VendorOrders.Models
 {
@@ -7,22 +10,37 @@ namespace VendorOrders.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
     private static List<Vendor> _instances = new List<Vendor>() {};
     public int Id { get; }
     public List<Order> Orders { get; set; }
+    public Dictionary<int,DateTime> OrderDates { get; set;}
+    public Dictionary<int,DateTime> SortedDates { get; set;}
 
-    public Vendor(string name, string description)
+    public Vendor(string name, string description, string address, string phone, string email)
     {
       Name = name;
       Description = description;
+      Address = address;
+      PhoneNumber = phone;
+      Email = email;
       _instances.Add(this);
       Id = _instances.Count;
       Orders = new List<Order>{};
+      OrderDates = new Dictionary<int, DateTime>{};
+      SortedDates = new Dictionary<int, DateTime>{};
     }
 
     public static List<Vendor> GetAll()
     {
       return _instances;
+    }
+
+    public static int VendorCount()
+    {
+      return _instances.Count;
     }
 
     public static void ClearAll()
@@ -38,6 +56,70 @@ namespace VendorOrders.Models
     public void AddOrder(Order order)
     {
       Orders.Add(order);
+      OrderDates.Add(order.Id,Convert.ToDateTime(order.Date));
+      SortedDates = OrderDates.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    public static void LoadInitialData()
+    {
+      using (StreamReader jsonData = new StreamReader("wwwroot/SeedData.json"))
+      {
+        List<Vendor> initialVendors = JsonConvert.DeserializeObject<List<Vendor>>(jsonData.ReadToEnd());
+
+        foreach (Vendor vendor in initialVendors)
+        {
+          foreach (Order order in vendor.Orders)
+          {
+            vendor.OrderDates.Add(order.Id,StringToDate(order.Date));
+            vendor.SortedDates = vendor.OrderDates.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+          }
+        }
+      }
+    }
+
+    public static DateTime StringToDate(string dateString)
+    {
+      string[] dateAsStrings = new string[]{};
+      bool dateMDYFormat;
+      if (dateString.Contains("-"))
+      {
+        dateAsStrings = dateString.Split("-");
+        dateMDYFormat = true;
+      }
+      else if (dateString.Contains("/"))
+      {
+        dateAsStrings = dateString.Split("/");
+        dateMDYFormat = false;
+      }
+      else
+      {
+        Console.WriteLine("Error in datetime from: " + dateString);
+        dateMDYFormat = false;
+      }
+      List<int> dateAsInts = new List<int>();
+      foreach (string datePart in dateAsStrings)
+      {
+        bool conversionSuccessful = Int32.TryParse(datePart, out int intDate);
+        if (conversionSuccessful == true)
+        {
+          dateAsInts.Add(intDate);
+        }
+      }
+      int[] dateNumbers = dateAsInts.ToArray();
+      DateTime convertedDate;
+      if (dateNumbers.Length == 3 && dateMDYFormat == true)
+      {
+        convertedDate = new DateTime(dateNumbers[2], dateNumbers[0], dateNumbers[1]);
+      }
+      else if (dateNumbers.Length == 3 && dateMDYFormat == false)
+      {
+        convertedDate = new DateTime(dateNumbers[0], dateNumbers[1], dateNumbers[2]);
+      }
+      else
+      {
+        convertedDate = DateTime.Today;
+      }
+      return convertedDate;
     }
   }
 }
